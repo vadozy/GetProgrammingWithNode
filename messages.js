b7d28baa5adc0d4c2d6f7aa1d20dfa4037b2bf73
Vadim Storozhuk <vadim.storo@gmail.com>
2018-08-12 23:17:06 -0400

    First commit

---
+++ messages.js
@@ -0,0 +1,4 @@
+exports.messages = ['You are great!',
+  'You can accomplish anything!',
+  'Success is in your future!'
+];
---
+++ recipe_connection/main.js
@@ -0,0 +1,3 @@
+const cities = require( 'cities' );
+let myCity = cities.zip_lookup('02114');
+console.log(myCity);
